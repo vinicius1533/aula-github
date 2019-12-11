79923e7b783c59433c3b602246f247eef496428c
vinicius da silva pires <viniciuskul599@gmail.com>
2019-12-10 21:24:58 -0300

    mensagem de boa noite

--- src/aplication/Progam.java
+++ src/aplication/Progam.java
@@ -1,11 +1,8 @@
 package aplication;
-
 public class Progam {
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		System.out.println("Bom dia!");
-
+		System.out.println("Boa tarde!");
+		System.out.println("Boa noite!");
 	}
-
 }
